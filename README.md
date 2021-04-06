73ec3ece2fa057c3dc576acf60e70a71a0a87ef6
fepauly <fe-pauly@t-online.de>
2021-04-06 16:24:46 +0200

    Adding english comments

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Misc Repository
-Some university projects.
+Some university projects. 
+Repo created while learning Julia.
 
 ## Algorithms
 Code snippets from the course "Algorithmen und Programmieren" in Julia.
+
--- algorithms/deflection.jl
+++ algorithms/deflection.jl
@@ -1,20 +1,20 @@
 #= Solve a numerical problem in Julia
 
-Biegelinie -> Balken mit Streckenlast
+deflection curve -> beam with area force
 
 =#
 
 using LinearAlgebra, Plots
 
-l = 1. # Länge des Balken
-q0 = 200. # Streckenlast
-E = 2-1e11 # E-Modul
-J = 5.4e-7 # Flächenträgheitsmoment
+l = 1. # length of beam
+q0 = 200. # area force
+E = 2-1e11 # Young’s modulus
+J = 5.4e-7 # geometrical moment of inertia
 
-n = 1000 # Anzahl Aufpunkte
+n = 1000 # number of points
 dx = l/(n-1)
 
-# Initialisierung der GLS
+# Initialize
 x = [i*dx for i in 0:n-1]
 d = [-2. for i in 0:n-1]
 a = [1. for i in 0:n-2]
@@ -23,15 +23,18 @@ b = [1. for i in 0:n-2]
 r_func(x) = (q0 * x * l * dx^2)/(6*E*J) * ((x/l)^2-1)
 r = [r_func(i) for i in x]
 
-# Randbedinungen
+# Border Conditions
 a[1] = 0; 
 b[n-1] = 0
 d[1] = 1; d[n] = 1
 r[1] = 0; r[n] = 0
 
+# Create system of equations
 tridiag = Tridiagonal(b, d, a)
 # display(tridiag)
 
+# Solve it
 w = tridiag\r
 
+# Simple plotting of the result
 plot(x, w, label="Biegelinie")
